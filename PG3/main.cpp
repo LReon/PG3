847727fdb001f21b4de25ec9c4bff4d4d9b0b05d
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-10-09 23:41:12 +0900

    k

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,8 +1,24 @@
-﻿#include <stdio.h>
+#include <stdio.h>
+
+int Recursive(int hours) {
+
+	if (hours <= 1) {
+		return 100;
+	}
+	
+	return (hours * Recursive(hours * 2 - 50));
+	
+};
+
 
 int main() {
 
-	printf("プロジェクトセカイ");
+	int hours = 3;
+	int result;
+
+	result = Recursive(hours);
+
+	printf("%d = %d", hours, result);
 
 	return 0;
 }
